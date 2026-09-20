9e095b6adee342d0b4457564c7366b0503632140
tata-artha26 <xxxdwiartha23@gmail.com>
2026-09-21 02:29:59 +0800

    Implementasikan pengambilan produk di HomeController untuk tampilan beranda.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,13 +1,21 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers;
 
+use App\Data\ProdukDummy;
 use Illuminate\View\View;
 
 class HomeController extends Controller
 {
     public function __invoke(): View
     {
-        return view('home');
+        $produk = ProdukDummy::tersedia();
+
+        return view('home', [
+            'produkPopuler' => array_slice($produk, 0, 4),
+            'produkTerbaru' => array_slice($produk, 4, 4),
+        ]);
     }
 }
